13078efe6ce1773fc945e1fa9f42ce0630f39c4f
Code Sandbox <hello@codesandbox.io>
2021-07-18 14:12:39 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fcaeb024b3c0fcc93da211ca1b57a05d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f3d0db53d2d9b124598bab17ffd40a",
+    "url": "https://joshdixo.github.io/csb-oh0vc/index.html"
+  },
+  {
+    "revision": "9876e6fd3cde9796e42d",
+    "url": "https://joshdixo.github.io/csb-oh0vc/static/js/2.a63bfbb6.chunk.js"
+  },
+  {
+    "revision": "6d5ef99f1b85e0d0943d",
+    "url": "https://joshdixo.github.io/csb-oh0vc/static/js/main.f2b7e165.chunk.js"
+  },
+  {
+    "revision": "ea9c182d261473425070",
+    "url": "https://joshdixo.github.io/csb-oh0vc/static/js/runtime-main.8428b3bb.js"
+  }
+]);
